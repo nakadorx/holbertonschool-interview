2a8462c02f4b11d9dd9e9ee51b532c9f281c9755
Nakador <nakadorx@gmail.com>
2020-12-10 22:28:09 +0100

    test

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""lock box"""
-
-
 def canUnlockAll(boxes):
     if boxes[0] == [] or not isinstance(boxes, list):
         return False
     done = [0]
-    for idx, box in enumerate(boxes):
+    for i, box in enumerate(boxes):
         for k in box:
-            if k != idx and k not in done and k in range(0, len(boxes)):
+            if k != i and k not in done and k in range(0, len(boxes)):
                 done.append(k)
     return len(done) == len(boxes)
