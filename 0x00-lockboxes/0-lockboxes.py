cb799f476604ae87500da0725a3cf56b99850d0a
Nakador <nakadorx@gmail.com>
2020-12-10 22:26:44 +0100

    test

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,20 +5,9 @@
 def canUnlockAll(boxes):
     if boxes[0] == [] or not isinstance(boxes, list):
         return False
-    keys = [0]
-    opened = [0]
-    new_key = True
-    while new_key:
-        new_key = False
-        for box in boxes:
-            if boxes.index(box) in keys:
-                if boxes.index(box) not in opened:
-                    opened.append(boxes.index(box))
-                for k in box:
-                    if k not in keys:
-                        keys.append(k)
-                        new_key = True
-    if len(opened) == len(boxes):
-        return True
-    else:
-        return False
+    done = [0]
+    for idx, box in enumerate(boxes):
+        for k in box:
+            if k != idx and k not in done and k in range(0, len(boxes)):
+                done.append(k)
+    return len(done) == len(boxes)
