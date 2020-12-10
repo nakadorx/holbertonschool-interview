67ea3e6f66df357c49615bf34e37fd2d005c3d02
Nakador <nakadorx@gmail.com>
2020-12-10 22:21:57 +0100

    test

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
+"""lock box"""
+
+
 def canUnlockAll(boxes):
-    verified = True
-    pw = [0]
-    done = [0]
-    while verified:
-        verified = False
+    keys = [0]
+    opened = [0]
+    new_key = True
+    while new_key:
+        new_key = False
         for box in boxes:
-            if boxes.index(box) in pw:
-                if boxes.index(box) not in done:
-                    done.append(boxes.index(box))
-                for t in box:
-                    if t not in pw:
-                        pw.append(t)
-                        verified = True
-    if len(boxes) == len(done):
+            if boxes.index(box) in keys:
+                if boxes.index(box) not in opened:
+                    opened.append(boxes.index(box))
+                for k in box:
+                    if k not in keys:
+                        keys.append(k)
+                        new_key = True
+    if len(opened) == len(boxes):
         return True
     else:
         return False
