e14842d3f61ea942c10382dccec2d8fa953429cb
nakadorx <nakadorx@gmail.com>
2020-12-23 21:57:12 +0100

    all

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,24 @@
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - hlbrtn C
+ * @parent: node
+ * @value: int
+ *
+ * Return: Always 0.
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *n;
+
+	n = malloc(sizeof(binary_tree_t));
+	if (n == NULL)
+		return (NULL);
+	n->parent = parent;
+	n->n = value;
+    n->right = NULL;
+	n->left = NULL;
+	return (n);
+}
