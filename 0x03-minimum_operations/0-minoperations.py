b7c84797add12d95c0eae34c9dce40a6bc8df19e
Nakador <nakadorx@gmail.com>
2021-01-07 18:23:17 +0100

    all

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -8,12 +8,12 @@ def minOperations(n):
     """
     if not isinstance(n, int) or n <= 1:
         return 0
-    res = 0
-    i = 2
+    x = 2
+    result = 0
     while n > 1:
-        if n % i == 0:
-            res += i
-            n /= i
+        if not(n % x == 0):
+            x += 1
         else:
-            i += 1
-    return res
+            result += x
+            n /= x
+    return result
