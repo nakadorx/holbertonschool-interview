76077e544fabba30c7ad3e24095579030aba446a
nakadorx <nakadorx@gmail.com>
2021-01-28 22:15:23 +0100

    all

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,33 +1,34 @@
+
 #!/usr/bin/python3
-"""holb"""
+"""holbb"""
 
 
 import sys
 
-cnt = 0
-dv = [0, 0, 0, 0, 0, 0, 0, 0]
-lx = ['200', '301', '400', '401', '403', '404', '405', '500']
-da = 0
+i = 0
+lis = ['200', '301', '400', '401', '403', '404', '405', '500']
+su = 0
+new = [0, 0, 0, 0, 0, 0, 0, 0]
 
 try:
     for line in sys.stdin:
         s = line.split()
         if len(s) > 2:
-            if s[-2] in lx:
-                x = lx.index(s[-2])
-                dv[x] = dv[x] + 1
-            cnt += 1
-            da = da + int(s[-1])
-        if cnt == 10:
-            cnt = 0
-            print("File size: {}".format(da))
+            if s[-2] in lis:
+                x = lis.index(s[-2])
+                new[x] = new[x] + 1
+            i += 1
+            su = su + int(s[-1])
+        if i == 10:
+            i = 0
+            print("File size: {}".format(su))
             for x in range(8):
-                if dv[x] != 0:
-                    print('{}: {}'.format(lx[x], dv[x]))
+                if new[x] != 0:
+                    print('{}: {}'.format(lis[x], new[x]))
 except Exception:
     pass
 finally:
     print("File size: {}".format(su))
     for x in range(8):
-        if dv[x] != 0:
-            print("{}: {}".format(lis[x], dv[x]))
+        if new[x] != 0:
+            print("{}: {}".format(lis[x], new[x]))
