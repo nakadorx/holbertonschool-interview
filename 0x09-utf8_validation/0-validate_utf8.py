c4329e5c633229a760a426f637f0189da5928364
nakadorx <nakadorx@gmail.com>
2021-02-18 19:49:47 +0100

    all

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,8 +3,7 @@
 
 def validUTF8(data):
     """utf8"""
-    tab = [i & 0b11111111 for i in data]
-    num = bytes(tab)
+    num = bytes([i & 0b11111111 for i in data])
     try:
         num.decode()
     except UnicodeDecodeError:
