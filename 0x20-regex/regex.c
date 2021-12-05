fd3da0f85747e411961ce8e2aee8f3e97529a928
nakadorx <nakadorx@gmail.com>
2021-12-05 10:30:57 +0100

    all

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -1,18 +1,28 @@
 #include "regex.h"
 /**
- * regex_match - holb
- * @str: str
- * @pattern: pat
- *
- * Return: return 1
- */
+* regex_match - holb
+* @str: string
+* @pattern: pattern
+* Return: 1
+*/
 int regex_match(char const *str, char const *pattern)
 {
-	if (!*str)
-		return (!*pattern || (pattern[1] == '*' && regex_match(str, pattern + 2)));
-	if (pattern[1] == '*')
-		return ((*pattern == '.' || *str == *pattern)
-			 ? (regex_match(str + 1, pattern) || regex_match(str, pattern + 2))
-			 : (regex_match(str, pattern + 2)));
-	return ((*pattern == '.' || *pattern == *str) && regex_match(str + 1, pattern + 1));
+	int a = 0;
+	int y = 0;
+
+	if (!str || !pattern)
+		return (0);
+
+	a = *str && (*str == *pattern || *pattern == '.');
+	b = *(pattern + 1) == '*';
+
+	if (!*str && !b)
+		return (*pattern ? 0 : 1);
+	else if (a && b)
+		return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+	else if (a && !b)
+		return (regex_match(str + 1, pattern + 1));
+	else if (b)
+		return (regex_match(str, pattern + 2));
+	return(0);
 }
